d61dc23325829d46a6c37ffdcabb2444cca8063b
noyonalways <noyonrahman2003@gmail.com>
2024-04-26 11:13:39 +0600

    test rating component

--- frontend/mui-demo/src/App.jsx
+++ frontend/mui-demo/src/App.jsx
@@ -1,8 +1,28 @@
-import { Favorite, FavoriteBorder } from "@mui/icons-material";
-import { Checkbox, Container } from "@mui/material";
+import { Favorite, FavoriteBorder, Star } from "@mui/icons-material";
+import { Box, Checkbox, Container, Rating } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
+import { useState } from "react";
+
+const labels = {
+  0.5: "Useless",
+  1: "Useless+",
+  1.5: "Poor",
+  2: "Poor+",
+  2.5: "Ok",
+  3: "Ok+",
+  3.5: "Good",
+  4: "Good+",
+  4.5: "Excellent",
+  5: "Excellent+",
+};
+
+function getLabelText(value) {
+  return `${value} Star${value !== 1 ? "s" : ""}, ${labels[value]}`;
+}
 
 const App = () => {
+  const [value, setValue] = useState(2);
+  const [hover, setHover] = useState(-1);
   return (
     <>
       <CssBaseline />
@@ -25,6 +45,24 @@ const App = () => {
           }}
           color="success"
         />
+        <Box display={"flex"} alignItems={"center"}>
+          <Rating
+            name="hover-feedback"
+            value={value}
+            precision={0.5}
+            getLabelText={getLabelText}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            onChangeActive={(event, newHover) => {
+              setHover(newHover);
+            }}
+            emptyIcon={<Star style={{ opacity: 0.55 }} fontSize="inherit" />}
+          />
+          {value !== null && (
+            <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+          )}
+        </Box>
       </Container>
     </>
   );
